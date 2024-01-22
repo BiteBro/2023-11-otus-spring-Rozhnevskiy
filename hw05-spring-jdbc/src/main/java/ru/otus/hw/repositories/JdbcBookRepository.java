7c4f58a16e5ee59d33fed33e20ae6af39b93a286
Rozhnevskiy Alexey <ralewww@gmail.com>
2024-01-23 00:20:22 +0300

    Fixed the error that the checkstyle plugin complains about

--- hw05-spring-jdbc/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ hw05-spring-jdbc/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -13,7 +13,11 @@ import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Repository
