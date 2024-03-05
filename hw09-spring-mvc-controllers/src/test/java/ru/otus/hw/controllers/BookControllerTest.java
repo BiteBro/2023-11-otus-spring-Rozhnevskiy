f70e7f81c0a53910011b433bb9355f6234ec1750
Rozhnevskiy Alexey <ralewww@gmail.com>
2024-03-06 02:13:23 +0300

    Added resources for tests and create BookControllerTest class

---
+++ hw09-spring-mvc-controllers/src/test/java/ru/otus/hw/controllers/BookControllerTest.java
@@ -0,0 +1,9 @@
+package ru.otus.hw.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import ru.otus.hw.controller.BookController;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+}
